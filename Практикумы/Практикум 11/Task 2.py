bc6592c49bae64f120232fa2c6a66a78a0228b51
dattilaa <alexander.dak@gmail.com>
2025-09-16 09:54:37 +0700

    Update Task 2.py

--- Практикумы/Практикум 11/Task 2.py
+++ Практикумы/Практикум 11/Task 2.py
@@ -1,6 +1,13 @@
-f = open('input.txt', 'r', encoding='utf-8')
-r = open('output.txt', 'w', encoding='utf-8')
-for line in f:
-    line = line.strip()
-    if line.startswith('a') or line.startswith('A'):
-        r.write(line + '\n')
+# Task 2
+line = input()
+counter = 0
+max_count = 0
+
+for i in range(1, len(line)):
+    if line[i] == line[i-1]:
+        counter += 1
+    else:
+        if counter > max_count:
+            max_count, counter = counter, 0
+
+print(max_count)
