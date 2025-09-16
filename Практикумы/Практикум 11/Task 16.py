35596509af3c95cd6d65897a916b9caab5ae5231
dattilaa <alexander.dak@gmail.com>
2025-09-16 10:02:39 +0700

    Create Task 16.py

---
+++ Практикумы/Практикум 11/Task 16.py
@@ -0,0 +1,14 @@
+# Task 16
+def check_chars(text):
+    chars = []
+    for s in text:
+        if s == '(':
+            chars.append(s)
+        elif s == ')':
+            if not chars:
+                return False
+            chars.pop()
+
+    return not len(chars)
+
+print(check_chars(input()))
