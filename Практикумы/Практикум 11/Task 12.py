a1ed706203d482fc0dc80638e97007af6d0b416a
dattilaa <alexander.dak@gmail.com>
2025-09-16 10:00:31 +0700

    Create Task 12.py

---
+++ Практикумы/Практикум 11/Task 12.py
@@ -0,0 +1,22 @@
+# Task 12
+import keyword as kw
+
+alphabet = [chr(i) for i in range(97, 123)]
+nums = [str(i) for i in range(10)]
+print(nums)
+symbols = alphabet + nums
+symbols.append('_')
+name = input()
+
+if not kw.iskeyword(name) and name[0] not in nums:
+    print(name[0])
+    flag = True
+    for i in name[1:]:
+        if i not in symbols:
+            flag = False
+            print('Не может быть именем')
+            break
+    if flag:
+        print('Может быть именем')
+else:
+    print('Не может быть именем')
