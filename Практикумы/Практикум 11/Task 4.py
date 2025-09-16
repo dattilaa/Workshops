8429a37887484a198d734e7d8c7904e83f6f6168
dattilaa <alexander.dak@gmail.com>
2025-09-16 09:55:56 +0700

    Update Task 4.py

--- Практикумы/Практикум 11/Task 4.py
+++ Практикумы/Практикум 11/Task 4.py
@@ -1,5 +1,6 @@
-f = open('input.txt', 'r', encoding='utf-8')
-o = open('output.txt', 'w', encoding='utf-8')
-for line in f:
-    if len(line) > 20:
-        o.write(line)
+# Task 4
+text = input()
+for i in text:
+    if text.count(i) == 3:
+        print(i)
+        break
